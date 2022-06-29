14bd48ce5c590fdbe4b9ee2f5c796d66979827d5
Alfredo RG <razogarduno@gmail.com>
2022-06-28 19:25:49 -0500

    Google Sign In

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -46,7 +47,48 @@ const login = async (req, res = response) => {
 
 }
 
+const googleSignIn = async (req, res = response) => {
+    const { id_token } = req.body;
+    try {
+        const { nombre, img, correo } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario){
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: 'USER_ROLE',
+                google: true
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if(!usuario.estado){
+            res.status(401).json({
+                msg: ' Hable con el administrador usuario bloqueado'
+            });
+        }
+
+        const token = await generarJWT(usuario.id);
+        res.json({
+            token,
+            usuario
+        });
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
